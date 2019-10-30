c7972ec8238b7571b1b00244219ac133f5980f84
John Bradley <johnbradley2008@gmail.com>
2019-10-30 10:42:49 -0400

    better comment create_gene_summary.R

--- code/create_gene_summary.R
+++ code/create_gene_summary.R
@@ -8,6 +8,7 @@ library(feather)
 gene_names_url <- "https://www.genenames.org/cgi-bin/download/custom?col=gd_hgnc_id&col=gd_app_sym&col=gd_app_name&col=gd_prev_sym&col=gd_aliases&col=gd_pub_chrom_map&col=gd_pub_refseq_ids&col=md_eg_id&col=gd_locus_type&col=md_mim_id&col=md_prot_id&status=Approved&hgnc_dbtag=on&order_by=gd_app_sym_sort&format=text&submit=submit"
 gene_summary_output_filename <- "gene_summary.feather"
 
+# returns a data frame based on gene_names_url and summaries from entrez "gene" database.
 build_gene_summary <- function(gene_names_url, entrez_key) {
   if (!is.null(entrez_key)) {
     set_entrez_key(entrez_key)
@@ -25,6 +26,7 @@ build_gene_summary <- function(gene_names_url, entrez_key) {
     drop_na(ncbi_gene_id_supplied_by_ncbi) %>%
     pull(ncbi_gene_id_supplied_by_ncbi)
 
+  # fetch sum
   fetched_cnt <- 0
   for (ncbi_gene_id_supplied_by_ncbi in ids) {
     gene_summary <- entrez_summary(db="gene", id=ncbi_gene_id_supplied_by_ncbi)
@@ -36,6 +38,7 @@ build_gene_summary <- function(gene_names_url, entrez_key) {
     entrez <- entrez %>%
       bind_rows(tmp)
     fetched_cnt <- fetched_cnt + 1
+    # Show a message every 1000 so a user can monitor progress
     if (fetched_cnt %% 1000 == 0) {
       message("Fetched ", fetched_cnt)
     }
@@ -49,7 +52,6 @@ build_gene_summary <- function(gene_names_url, entrez_key) {
   gene_summary$locus_type <- str_to_sentence(gene_summary$locus_type)
   gene_summary$locus_type <- str_replace(gene_summary$locus_type, "Rna", "RNA")
   gene_summary$approved_name <- str_to_sentence(gene_summary$approved_name)
-
   gene_summary %>%
     unite("aka", previous_symbols:synonyms, sep = ", ", na.rm = TRUE)
 }
@@ -59,15 +61,18 @@ create_gene_summary <- function(gene_names_url, entrez_key, gene_summary_output_
   write_feather(gene_summary, path = gene_summary_output_path)
 }
 
+# Command line argument parser that will let a user optionally specify:
+# - entrez key to speed up fetching data. To create an entrez key see "Using API Keys" at https://cran.r-project.org/web/packages/rentrez/vignettes/rentrez_tutorial.html
+# - alternate destination directory
 option_list = list(
   make_option(c("--entrezkey"), type="character", default=NULL,
               help="NCBI entrez key [default= %default]", metavar="character"),
   make_option(c("--destdir"), type="character", default="data",
               help="Destination directory [default= %default]", metavar="character")
 )
-
 opt_parser = OptionParser(option_list=option_list)
 opt = parse_args(opt_parser)
 
+# fetch data and write gene summary feather file
 create_gene_summary(gene_names_url, opt$entrezkey,
                     here::here(opt$destdir, gene_summary_output_filename))
