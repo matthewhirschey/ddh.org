da59eee24eb82d653bedeb66f8b745e930fbaf2d
hirscheylab <matthew.hirschey@duke.edu>
2019-12-09 16:42:47 -0500

    replace Rmd with R scripts

--- code/quarterly_release.R
+++ code/quarterly_release.R
@@ -5,12 +5,12 @@ library(here)
 source(here::here("code", "create_gene_summary.R"))
 
 ##run upon each release
-#script to generate ddh correlation matrix
-rmarkdown::render(input = here::here("code", "depmap_generate.Rmd"), output_dir = here::here("data"))
-#file.remove() could be used to programtically remove the file generated
+source(here::here("code", "generate_depmap_data.R")) #first script to generate ddh correlation matrix
+source(here::here("code", "generate_depmap_stats.R")) #second script to generate ddh stats
 
-#script to generate ddh stats
-rmarkdown::render(input = here::here("code", "depmap_generate_stats.Rmd"), output_dir = here::here("data"))
+#after running above two lines, then run these two in either order
+source(here::here("code", "generate_depmap_tables.R")) #third script to generate ddh tables
+source(here::here("code", "generate_depmap_pathways.R")) #fourth script to generate ddh pathways; needs ||
 
-#script to generate ddh pathways
-rmarkdown::render(input = here::here("code", "depmap_generate_pathways.Rmd"), output_dir = here::here("data"))
+#then rerun the methods Rmd document to generate source for methods.md
+rmarkdown::render(input = here::here("code", "methods.Rmd"))
