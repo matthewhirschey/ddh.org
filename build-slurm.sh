58d635c1dd8f41fca062d60b00aa8ab45331372d
John Bradley <johnbradley2008@gmail.com>
2019-12-23 15:04:50 -0500

    update slurm script to build in parallel

--- build-slurm.sh
+++ build-slurm.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #SBATCH --mem=32G
+#SBATCH --cpus-per-task=10
 #SBATCH --output=logs/ddh-%j.out
 
 source config.sh
@@ -7,4 +8,6 @@ export SINGULARITY_TMPDIR SINGULARITY_CACHEDIR ENTREZ_KEY DOCKER_IMG
 mkdir -p ${SINGULARITY_TMPDIR} ${SINGULARITY_CACHEDIR} ${SINGULARITY_IMAGEDIR}
 
 make container_image
-RSCRIPT_CMD="singularity exec singularity/images/depmap.sif Rscript" make
+export RSCRIPT_CMD="singularity exec singularity/images/depmap.sif Rscript"
+make gene_summary depmap_data depmap_stats depmap_tables
+make -j 10 depmap_pathways
