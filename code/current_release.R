acf6f80e1e43f60e3dd8485c7fc913fae1736f90
hirscheylab <matthew.hirschey@duke.edu>
2020-03-02 23:01:09 -0500

    Add find_threshold.R
    This new R script is to be run after updating the URLs for the new quarterly release data. This takes the URLs, and calculates a threshold to cut out genes with too many missing expression values from the essentiality scores. This then ranks the genes based on the highest number of missing values, and visualizes number of correlated genes as a surrogate for poor quality (large # = poor correlations). Visuals will be added to methods
    
    Addresses issue #69

--- code/current_release.R
+++ code/current_release.R
@@ -2,4 +2,4 @@ release <- "20Q1"
 achilles_url <- "https://ndownloader.figshare.com/files/21521910" #achilles_gene_effect.csv
 ccle_url <- "https://ndownloader.figshare.com/files/21521940" #CCLE_expression.csv
 cclemeta_url <- "https://ndownloader.figshare.com/files/21522000" #sample_info.csv
-na_cutoff <- 506
+na_cutoff <- 631 #~5% FDR
