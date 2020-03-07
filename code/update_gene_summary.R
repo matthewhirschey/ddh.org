4fef5d85403cc8faaf44049bbfc5219bf7edd869
matthewhirschey <matthew@hirschey.org>
2020-03-07 08:07:13 -0500

    Add update_gene_summary.R
    This commit adds a new script to run quarterly(?) which requires a gene_summary_raw.Rds file in the data directory. This is a copy of the current_gene_summmary.Rds file. Using this file, the code will add 4 new columns to this dataframe: pubmed_count, concept_count, and percent rank for each. These are static values, and the idea is to serve them either on the gene landing page, in the header of the similar/dissimilar gene dependency page, or both. These values will provide context for new data (columns) on these page which quantifies known relationships between genes via co-publication.

---
+++ code/update_gene_summary.R
@@ -0,0 +1,56 @@
+##This script updates gene_summary to include pubmed data
+
+#load libraries
+library(tidyverse)
+library(here)
+library(janitor)
+
+#rm(list=ls()) 
+
+time_begin_gene_summary<- Sys.time()
+
+#import
+gene_summary_raw <- readRDS(here::here("data", "gene_summary_raw.Rds"))
+
+gene2pubmedurl <- "ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene2pubmed.gz"
+gene2pubmed_raw <- read_tsv(gene2pubmedurl, col_names = TRUE) %>% 
+  clean_names()
+
+#add pubmed_count to gene_summary
+gene_summary <- gene2pubmed_raw %>% 
+  filter(number_tax_id == 9606) %>%  #only the rows corresponding to humans (#tax_id = 9606) 
+  group_by(gene_id) %>% 
+  count(sort = TRUE) %>% 
+  right_join(gene_summary_raw, by = c("gene_id" = "ncbi_gene_id"))  %>% 
+  rename("ncbi_gene_id" = "gene_id", 
+         "pubmed_count" = "n") %>% 
+  mutate(pubmed_count = replace_na(pubmed_count, 0)) %>% 
+  ungroup()
+
+#add concept count
+pubtatorurl <- "ftp://ftp.ncbi.nlm.nih.gov/pub/lu/PubTatorCentral/gene2pubtatorcentral.gz"
+gene2pubtator <- read_tsv(pubtatorurl, col_names = c("pmid", "type", "concept_id", "mentions", "resource")) %>% 
+  select(pmid, concept_id, mentions) %>% 
+  filter(concept_id %in% gene_summary$ncbi_gene_id == TRUE)
+
+gene2pubtator$concept_id <- as.numeric(gene2pubtator$concept_id)
+
+gene_summary <- gene2pubtator %>% 
+  group_by(concept_id) %>% 
+  count(sort = TRUE) %>% 
+  right_join(gene_summary, by = c("concept_id" = "ncbi_gene_id"))  %>% 
+  rename("ncbi_gene_id" = "concept_id", 
+         "concept_count" = "n") %>% 
+  mutate(concept_count = replace_na(concept_count, 0)) %>% 
+  ungroup()
+
+gene_summary <- gene_summary %>%
+  mutate(pubmed_count_rank = percent_rank(pubmed_count),
+         concept_count_rank = percent_rank(concept_count)) %>% 
+  select("approved_symbol", "approved_name", "aka", "ncbi_gene_id", "hgnc_id", "chromosome", "ref_seq_i_ds", "locus_type", "omim_id_supplied_by_omim", "uni_prot_id_supplied_by_uni_prot", "entrez_summary", "pubmed_count", "pubmed_count_rank", "concept_count", "concept_count_rank")
+
+#save final file
+saveRDS(gene_summary, here::here("data", "gene_summary.Rds"))
+
+#how long
+time_end_gene_summary <- Sys.time()
