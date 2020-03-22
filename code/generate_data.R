aff05d62bb13e2d974125c7eecec9e3a2be30ba6
John Bradley <johnbradley2008@gmail.com>
2020-03-22 10:20:00 -0400

    name data generation steps

--- code/generate_data.R
+++ code/generate_data.R
@@ -1,15 +1,22 @@
+## Generates all data needed for running the shiny app
+## Environment variable DDH_STEP can be used to control which steps are run.
+## The step names are "gens", "pubm", "tbls", "path". They need to be run in this order.
+## By default all steps are run in the correct order. These steps are broken up based on 
+## Their different resource requirments.
+
 library(here)
 source(here::here("code", "current_release.R"))
 
 message("Generating data for ", release, ".")
 
 # read DDH_STEP environment variable to get steps to run or the default which is all steps.
-steps_to_run = unlist(strsplit(Sys.getenv("DDH_STEP", "1,2,3,4"), ','))
+steps_to_run = unlist(strsplit(Sys.getenv("DDH_STEP", "gens,pubm,tbls,path"), ','))
 # read ENTREZ_KEY from environment variable
 entrez_key = Sys.getenv("ENTREZ_KEY")
 
-if ("1" %in% steps_to_run) {
-  message("DDH: Running step 1.")
+if ("gens" %in% steps_to_run) {
+  # Requires around 64G of memory
+  message("DDH: Running step 1 - Gene summary and other files.")
   source(here::here("code", "create_gene_summary.R"))
   create_gene_summary(gene_names_url, entrez_key, here::here("data", gene_summary_output_filename))
 
@@ -19,20 +26,23 @@ if ("1" %in% steps_to_run) {
   message("DDH: Finished step 1.")
 }
 
-if ("2" %in% steps_to_run) {
-  message("DDH: Running step 2.")
+if ("pubm" %in% steps_to_run) {
+  # Requires around 212G of memory
+  message("DDH: Running step 2 - pubmed data.")
   source(here::here("code", "generate_pubmed_data.R")) #script to generate pubtator relationships
   message("DDH: Finished step 2.")
 }
 
-if ("3" %in% steps_to_run) {
-  message("DDH: Running step 3.")
+if ("tbls" %in% steps_to_run) {
+  # Requires 32G of memory
+  message("DDH: Running step 3 - ddh tables.")
   source(here::here("code", "generate_depmap_tables.R")) #third script to generate ddh tables
   message("DDH: Finished step 3.")
 }
 
-if ("4" %in% steps_to_run) {
-  message("DDH: Running step 4.")
+if ("path" %in% steps_to_run) {
+  # Requires 32G of memory and 10 CPUs
+  message("DDH: Running step 4 - pathway data generation.")
   source(here::here("code", "generate_depmap_pathways.R"))
   num_subset_files <- 10
 
--- submit-slurm-jobs.sh
+++ submit-slurm-jobs.sh
@@ -6,12 +6,20 @@ STEP1_FLAGS="--mem=64G"
 STEP2_FLAGS="--mem=212G"
 STEP3_FLAGS="--mem=32G"
 STEP4_FLAGS="--mem=32G --cpus-per-task=10"
+STD_FLAGS="--parsable"
+if [ -v "$DDH_EMAIL" ]
+then
+  STD_FLAGS="$STD_FLAGS --mail-type=DONE --mail-user=$DDH_EMAIL"
+fi
 
 # run data generation steps with each waiting for the previous to finish
-STEP1_JOB_ID=$(sbatch --parsable --job-name="ddh-step1" $STEP1_FLAGS run-slurm-step.sh "1")
-STEP2_JOB_ID=$(sbatch --parsable --job-name="ddh-step2" --dependency=afterok:$STEP1_JOB_ID $STEP2_FLAGS run-slurm-step.sh "2")
-STEP3_JOB_ID=$(sbatch --parsable --job-name="ddh-step3" --dependency=afterok:$STEP2_JOB_ID $STEP3_FLAGS run-slurm-step.sh "3")
-STEP4_JOB_ID=$(sbatch --parsable --job-name="ddh-step4" --dependency=afterok:$STEP3_JOB_ID $STEP4_FLAGS run-slurm-step.sh "4")
+STEP1_JOB_ID=$(sbatch $STD_FLAGS --job-name="ddh-gens1" $STEP1_FLAGS run-slurm-step.sh "gens")
+STEP2_JOB_ID=$(sbatch $STD_FLAGS --job-name="ddh-pubm2" --dependency=afterok:$STEP1_JOB_ID $STEP2_FLAGS run-slurm-step.sh "pubm")
+STEP3_JOB_ID=$(sbatch $STD_FLAGS --job-name="ddh-tbls3" --dependency=afterok:$STEP2_JOB_ID $STEP3_FLAGS run-slurm-step.sh "tbls")
+STEP4_JOB_ID=$(sbatch $STD_FLAGS --job-name="ddh-path4" --dependency=afterok:$STEP3_JOB_ID $STEP4_FLAGS run-slurm-step.sh "path")
 
-# upload results when last data generation step finishes
-sbatch --job-name="ddh-upload" --dependency=afterok:$STEP4_JOB_ID upload-slurm.sh
+if [ -v "$DDH_UPLOAD_RESULTS" ]
+then
+   # upload results when last data generation step finishes
+   sbatch $STD_FLAGS --job-name="ddh-upload" --dependency=afterok:$STEP4_JOB_ID upload-slurm.sh
+fi
