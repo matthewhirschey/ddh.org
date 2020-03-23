9def92dfbef57f8c13233f4b97625fcc6504539c
matthewhirschey <matthew@hirschey.org>
2020-03-23 16:09:24 -0400

    create_pathways from URL
    this updates the R script to get pathways from a URL (currently enrichr website)

--- code/create_pathways.R
+++ code/create_pathways.R
@@ -2,15 +2,22 @@ library(tidyverse)
 library(here)
 library(readxl)
 library(janitor)
+library(vroom)
 
-go_bp <- read_xls(here::here("data", "GO_Biological_Process_2018.xls"), col_names = FALSE, .name_repair = "universal") %>% 
-  clean_names() %>%  
+#rm(list = ls())
+
+go_bp_url <- "https://amp.pharm.mssm.edu/Enrichr/geneSetLibrary?mode=text&libraryName=GO_Biological_Process_2018"
+#consider updating from here: http://amigo.geneontology.org/amigo/software_list
+
+go_bp <- vroom(go_bp_url, col_names = FALSE, delim = "\t") %>%  #, col_names = FALSE, .name_repair = "universal") %>% 
+  clean_names() %>% 
   select(-x2) %>% 
-  #slice(1:10) %>% 
-  pivot_longer(cols = -x1, names_to = "gene", values_to = "gene_name") %>% 
-  filter(!is.na(gene_name)) %>% 
-  select(-gene) %>% 
+  unite(col = "gene", x3:x15, sep = "\t") %>% 
+  separate_rows(gene, sep = "\t") %>% 
   rename("pathway" = "x1") %>% 
+  filter(!is.na(gene), 
+         gene != "NA", 
+         gene != "") %>% 
   group_by(pathway)
 
 go_bp_count <- go_bp %>%  
@@ -20,4 +27,5 @@ go_bp_count <- go_bp %>%
 go_bp <- go_bp %>% 
   nest() %>% 
   left_join(go_bp_count, by = "pathway")
-  
+
+rm(go_bp_count)
