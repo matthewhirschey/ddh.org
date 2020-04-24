be37149cb35a1c8f96cc48334ba2d40e292881d5
matthewhirschey <matthew@hirschey.org>
2020-04-24 07:40:47 -0400

    add multi-gene capability to plots
    This plots more than one gene to both cell bins and cell deps plots. Need to re-envision cell deps plots for multiple genes.

--- code/fun_plots.R
+++ code/fun_plots.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(cowplot)
 library(viridis)
+library(plotly)
 
 make_cellbins <- function(table, expression_table, gene_symbol) {
   p <- table %>% #plot setup
@@ -12,10 +13,11 @@ make_cellbins <- function(table, expression_table, gene_symbol) {
     geom_vline(xintercept = 1, color = "lightgray") +
     geom_vline(xintercept = -1, color = "lightgray") +
     geom_vline(xintercept = 0) +
-    geom_histogram(aes(x = dep_score, fill = gene_symbol), binwidth = 0.25, color = "gray") + #fill = "#02224C"
-    labs(x = "Dependency Score (binned)") +
+    geom_density(aes(x = dep_score, 
+                     fill = fct_reorder(gene_symbol, dep_score, .fun = median), 
+                     text = paste0("Gene: ", gene_symbol)), alpha = 0.6, color = "gray") +
+    labs(x = "Dependency Score (binned)", fill = "Query \nGene", y = "Density") +
     scale_fill_viridis(discrete = TRUE, option = "D", direction = 1, guide = "legend") +
-    labs(fill = "Query Gene") +
     theme_cowplot()
   
   if(length(gene_symbol) == 1){
@@ -24,7 +26,7 @@ make_cellbins <- function(table, expression_table, gene_symbol) {
   } else {
     p
   }
-  return(p)
+  return(ggplotly(p, tooltip = c("text")))
 }
 
 make_celldeps <- function(data_table, expression_table, gene_symbol, mean) {
@@ -33,12 +35,14 @@ make_celldeps <- function(data_table, expression_table, gene_symbol, mean) {
     left_join(expression_table, by = "X1") %>%
     select(-X1) %>%
     pivot_longer(cols = !any_of(c("cell_line", "lineage")), names_to = "gene_symbol", values_to = "dep_score") %>% 
-    ggplot(aes(x = fct_reorder(cell_line, dep_score, .desc = FALSE), 
+    ggplot(aes(x = fct_reorder(cell_line, dep_score, .fun = max, .desc = FALSE), 
                y = dep_score, 
                text = paste0("Cell Line: ", cell_line), 
-               color = gene_symbol)) +
+               color = fct_reorder(gene_symbol, dep_score, .fun = max, .desc = TRUE)
+               )) +
     geom_point(alpha = 0.4) + #color = "#02224C"
-    labs(x = "Cell Lines", y = "Dependency Score", color = "Query Gene") +
+    #geom_boxplot() +
+    labs(x = "Cell Lines", y = "Dependency Score", color = "Query \nGene") +
     geom_hline(yintercept = mean) +
     geom_hline(yintercept = 1, color = "lightgray") +
     geom_hline(yintercept = -1, color = "lightgray") +
