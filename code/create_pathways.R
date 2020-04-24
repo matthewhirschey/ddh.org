9cc797f4006e5e84b02bf994963293793e9e1c6a
matthewhirschey <matthew@hirschey.org>
2020-04-24 07:50:06 -0400

    create pathway limit to 40

--- code/create_pathways.R
+++ code/create_pathways.R
@@ -27,12 +27,10 @@ go_bp_count <- go_bp %>%
 pathways <- go_bp %>% 
   nest() %>% 
   left_join(go_bp_count, by = "pathway") %>% 
-  filter(count < 50) %>%  #removes 839 (what is max number of genes you want to query?)
+  filter(count < 41) %>%  #5103 total; <50 removes 839; <30 removes 1462; <20 removes 2118
   separate(col = "pathway", into = c("pathway", "go"), sep = "\\(GO\\:") %>% 
-  separate(col = "go", into = "go", sep = "\\)", extra = "drop")
+  separate(col = "go", into = "go", sep = "\\)", extra = "drop") %>% 
+  mutate(pathway = str_trim(pathway, side = "right"))
 
 rm(go_bp_count)
 saveRDS(pathways, file = here::here("data", "pathways.Rds"))
-
-
-###get something back out
