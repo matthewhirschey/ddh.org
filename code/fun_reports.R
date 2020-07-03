e7574829fb34f880ceae5fcc6d5c8c1d09dfe930
John Bradley <johnbradley2008@gmail.com>
2020-07-03 10:16:29 -0400

    simplify report generation
    Renders report in reports directory.
    Updates OpenShift to create temporary directory for reports.
    Fixes #104

--- code/fun_reports.R
+++ code/fun_reports.R
@@ -74,7 +74,7 @@ render_complete_report <- function (file,
   dep_bottom <- make_bottom_table(bottomtable_data, gene_symbol)
   flat_bottom_complete <- make_enrichment_bottom(enrichmentbottom_data, gene_symbol)
   graph_report <- make_graph_report(toptable_data, bottomtable_data, gene_symbol)
-  rmarkdown::render(here::here("code", "report_app.Rmd"), output_file = file)
+  render_rmarkdown_in_tempdir(here::here("code", "report_app.Rmd"), output_file = file)
 }
 #render_complete_report(file = "tmp.pdf", gene_symbol = "SDHA", type = "gene")
 #render_complete_report(file = "tmp.pdf", gene_symbol = c("GHRHR", "CDH3", "GHRH", "IGF1", "PHIP", "WNT1", "GH1"), type = "pathway")
@@ -89,7 +89,7 @@ render_dummy_report <- function (file,
                           type, 
                           summary1, 
                           summary2)
-  rmarkdown::render(here::here("code", "report_dummy_app.Rmd"), output_file = file)
+  render_rmarkdown_in_tempdir(here::here("code", "report_dummy_app.Rmd"), output_file = file)
 }
 #render_dummy_report(file = "tmp.pdf", gene_symbol = "SDHA", type = "gene")
 
@@ -169,4 +169,23 @@ render_report_to_file <- function(file,
                                summary2)
     file.rename(out, file)
   }
+}
+
+render_rmarkdown_in_tempdir <- function(rmd_path, output_file, envir = parent.frame()) {
+  # make sure the base report directory exists
+  report_base_dir = here::here("report")
+  if (!file.exists(report_base_dir)) {
+    dir.create(report_base_dir)
+  }
+  # determine the filename of the Rmd file we will use for rendering
+  rmd_filename <- basename(rmd_path)
+  # create a temporary directory and make it our working directory
+  temp_dir <- tempfile(pattern="tmpdir", tmpdir=report_base_dir)
+  dir.create(temp_dir)
+  owd <- setwd(temp_dir)
+  on.exit(setwd(owd))
+  on.exit(unlink(temp_dir, recursive = TRUE))
+  # copy the Rmd file into our temporary(current) directory
+  file.copy(rmd_path, rmd_filename, overwrite = TRUE)
+  rmarkdown::render(rmd_filename, output_file = output_file, envir = envir)
 }
